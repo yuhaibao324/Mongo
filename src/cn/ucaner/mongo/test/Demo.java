25dd8a2ea17969e4f82ecbd98e2e624c66eadf19
GooogleX <jasonandy@mail.com>
2017-12-29 15:06:54 +0800

    init mongoDB

---
+++ src/cn/ucaner/mongo/test/Demo.java
@@ -0,0 +1,21 @@
+package cn.ucaner.mongo.test;
+
+/**
+* @Package：cn.ucaner.mongo.test   
+* @ClassName：Demo   
+* @Description：   <p> Demo 测试</p>
+* @Author： - DaoDou   
+* @CreatTime：2017年12月29日 下午2:58:59   
+* @Modify By：   
+* @ModifyTime：  2017年12月29日
+* @Modify marker：   
+* @version    V1.0
+ */
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("MongoDB ... ");
+	}
+
+}
